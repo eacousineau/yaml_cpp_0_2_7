26c6ce550b8acc571239e7645c02ed250d045a60
Eric Cousineau <eacousineau@gmail.com>
2015-05-31 10:51:49 -0700

    Update include guards

--- include/yaml-cpp-0.2.7/yaml.h
+++ include/yaml-cpp-0.2.7/yaml.h
@@ -1,5 +1,5 @@
-#ifndef YAML_H_62B23520_7C8E_11DE_8A39_0800200C9A66
-#define YAML_H_62B23520_7C8E_11DE_8A39_0800200C9A66
+# ifndef YAML_H_62B23520_7C8E_11DE_8A39_0800200C9A66_0_2_7 
+# define YAML_H_62B23520_7C8E_11DE_8A39_0800200C9A66_0_2_7 
 
 #if defined(_MSC_VER) || (defined(__GNUC__) && (__GNUC__ == 3 && __GNUC_MINOR__ >= 4) || (__GNUC__ >= 4)) // GCC supports "pragma once" correctly since 3.4
 #pragma once
@@ -14,4 +14,4 @@
 #include "yaml-cpp-0.2.7/stlemitter.h"
 #include "yaml-cpp-0.2.7/exceptions.h"
 
-#endif // YAML_H_62B23520_7C8E_11DE_8A39_0800200C9A66
+# endif // YAML_H_62B23520_7C8E_11DE_8A39_0800200C9A66_0_2_7 
